31be1dccd71fc6c9a2cb6f125b9323745efe1252
Lara <laramontava@gmail.com>
2016-06-02 19:31:07 +0000

    Listar usuarios bien hecho

--- modules/user/view/delete_user.php
+++ modules/user/view/delete_user.php
@@ -1,6 +1,6 @@
 <div class="container">
     <h3>Delete user</h3>
-    <form id="deleteuser" name="deleteuser" method="post" action="index.php?page=controller_user&op=delete">
+    <form id="deleteuser" name="deleteuser" method="post">
         <div>¿Seguro que quieres eliminar al usuario?</div>
         <input type="submit" name="confirmar_delete" value="Delete"/>
         <a class="btn" href="index.php?page=controller_user&op=list">Cancel</a>
--- modules/user/view/list_users.php
+++ modules/user/view/list_users.php
@@ -11,9 +11,20 @@
 		</thead>
 		<tbody>
 		    <?php
-		        /*$daosql = new DAOmysql();
-			    $daosql->list_users();*/
-			    
+			    foreach ($user as $row) {
+					echo '<tr>';
+					echo '<td>'. $row['user'] . '</td>';
+					echo '<td>'. $row['email'] . '</td>';
+					echo '<td>'. $row['mobile'] . '</td>';
+					echo '<td width=250>';
+					echo '<a class="btn" href="index.php?page=controller_user&op=read&user='.$row['user'].'">Read</a>'; 
+					echo '&nbsp;';
+					echo '<a class="btn btn-success" href="index.php?page=controller_user&op=update&user='.$row['user'].'">Update</a>';
+					echo '&nbsp;';
+					echo '<a class="btn btn-danger" href="index.php?page=controller_user&op=delete&user='.$row['user'].'">Delete</a>';
+					echo '</td>';
+					echo '</tr>';
+				}
 		    ?>
 		</tbody>
 		</table>
